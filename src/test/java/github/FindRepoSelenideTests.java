211be1cd5c34404b1d5b51cf2b0c9c59b9373f4c
Vladimir Sarychev <svmyhome@mail.ru>
2024-10-19 13:38:46 +0300

    fix

--- src/test/java/github/FindRepoSelenideTests.java
+++ src/test/java/github/FindRepoSelenideTests.java
@@ -1,16 +1,25 @@
 package github;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class FindRepoSelenideTests {
 
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize="1980x1200";
+        Configuration.baseUrl="https://github.com";
+        Configuration.pageLoadStrategy="eager";
+    }
     @Test
     void findJunitFromSelenideTest() {
-        open("https://github.com/selenide/selenide");
+        open("/selenide/selenide");
         $("a#wiki-tab").click();
         SelenideElement pages = $("#wiki-pages-filter");
         pages.click();
@@ -19,5 +28,18 @@ public class FindRepoSelenideTests {
         rightbar.shouldHave(text("SoftAssertions"));
         rightbar.find(byText("SoftAssertions")).click();
         $("#wiki-body").shouldHave(text("JUnit5"));
+        $("#wiki-body").shouldHave(text("""
+                @ExtendWith({SoftAssertsExtension.class})
+                class Tests {
+                  @Test
+                  void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
+                
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                  }
+                }
+                """));
     }
 }
