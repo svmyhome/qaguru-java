b0d9844677889c3cab2f4831a9d831e696f1151f
Vladimir Sarychev <svmyhome@mail.ru>
2024-11-13 09:18:43 +0300

    lesson 9 tests

--- src/test/java/model/jackson/MyJackson.java
+++ src/test/java/model/jackson/MyJackson.java
@@ -3,23 +3,41 @@ package model.jackson;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.List;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class MyJackson {
     @JsonProperty("ID")
     private Integer id;
     @JsonProperty("Name")
     private String name;
+    private List<String> skillList;
+    private Book book;
 
-    public MyJackson(Integer id, String name) {
-        this.id = id;
-        this.name = name;
+    public Book getBook() {
+        return book;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+
+    public Integer getId() {
+        return id;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getSkillList() {
+        return skillList;
+    }
+
+    @Override
+    public String toString() {
+        return "MyJackson1{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", skillList=" + skillList +
+                ", book=" + book +
+                '}';
     }
 }
