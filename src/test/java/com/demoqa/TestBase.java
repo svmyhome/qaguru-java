558ff732fdc7295146aa116a76351a41ef5c2492
Vladimir Sarychev <svmyhome@mail.ru>
2024-11-21 19:31:38 +0300

    add remote browser

--- src/test/java/com/demoqa/TestBase.java
+++ src/test/java/com/demoqa/TestBase.java
@@ -28,6 +28,7 @@ public class TestBase {
         Configuration.browserSize = "1500x1200";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @AfterEach
