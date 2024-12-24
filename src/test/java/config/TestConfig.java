c06e8fb988de1127d7058181106e58328ddbe53b
Vladimir Sarychev <svmyhome@mail.ru>
2024-12-24 09:46:41 +0300

    add test add book

--- src/test/java/config/TestConfig.java
+++ src/test/java/config/TestConfig.java
@@ -1,8 +1,13 @@
 package config;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 import static constants.Constants.RunVariables.server;
 
@@ -13,8 +18,21 @@ public class TestConfig {
     public static void setUP() {
         RestAssured.baseURI = server;
 
-        Configuration.browserSize = "1000x1500";
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
+        Configuration.browserSize = System.getProperty("browserSize", "1000x1900");
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = System.getProperty("remote");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
 }
