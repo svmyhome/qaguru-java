af4f53cd0f147a4bf6ec125c5bd438fe0a5ffb6e
Vladimir Sarychev <svmyhome@mail.ru>
2025-01-19 11:55:38 +0300

    add owner config android

--- src/test/java/drivers/BrowserStackDriver.java
+++ src/test/java/drivers/BrowserStackDriver.java
@@ -1,8 +1,10 @@
 package drivers;
 
 import com.codeborne.selenide.WebDriverProvider;
+import config.BrowserStackAndroidConfig;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 
@@ -14,7 +16,7 @@ public class BrowserStackDriver implements WebDriverProvider {
     @Nonnull
     @Override
     public WebDriver createDriver(@Nonnull Capabilities capabilities) {
-
+        BrowserStackAndroidConfig config = ConfigFactory.create(BrowserStackAndroidConfig.class, System.getProperties());
         UiAutomator2Options options = new UiAutomator2Options();
         options.setCapability("appium:app", "bs://sample.app");
         options.setCapability("appium:deviceName", "Samsung Galaxy S22 Ultra");
